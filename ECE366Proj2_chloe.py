f302d601b0d93b434949332c59c98d07d85c2a00
Chloetene Malone <cmalon5@uic.edu>
2019-09-30 14:41:47 -0500

    python - still need branch

--- ECE366Proj2_chloe.py
+++ ECE366Proj2_chloe.py
@@ -28,6 +28,8 @@ def main():
     PC = 0
     regNameInit(regName)
     regval = [0]*26 #0-23 and lo,hi
+    LO = 24
+    HI = 25
     f = open("mc.txt","w+")
     h = open("mips1.asm","r")
     asm = h.readlines()
@@ -37,50 +39,62 @@ def main():
     saveJumpLabel(asm,labelIndex,labelName, labelAddr) # Save all jump's destinations
     
     for line in asm:
+        
+        f.write('------------------------------ \n')
+        f.write('MIPS Instruction: ' + line + '\n')
+        
         line = line.replace("\n","") # Removes extra chars
         line = line.replace("$","")
         line = line.replace(" ","")
         line = line.replace("zero","0") # assembly can also use both $zero and $0
-
-        if(line[0:4] == "addi"): # ADDI, $t = $s + imm; advance_pc (4); addi $t, $s, imm
+            
+        if(line[0:5] == "addiu"): # $t = $s + imm; advance_pc (4); addiu $t, $s, imm
+            line = line.replace("addiu","")
+            line = line.split(",")
+            PC = PC + 4;
+            regval[int(line[0])] = regval[int(line[1])] + int(line[2])
+            f.write('Operation: $' + line[0] + ' = ' + '$' + line[1] + ' + ' + line[2] + '; ' + '\n')
+            f.write('PC is now at ' + str(PC) + '\n')
+            f.write('Registers that have changed: ' + '$' + line[0] + ' = ' + str(regval[int(line[0])]) + '\n')
+        
+        elif(line[0:4] == "addi"): # ADDI, $t = $s + imm; advance_pc (4); addi $t, $s, imm
             #f.write(line)
             line = line.replace("addi","")
             line = line.split(",")
+            PC = PC + 4;
             regval[int(line[0])] = regval[int(line[1])] + int(line[2])
-            f.write('$' + line[0] + ' = ' + '$' + line[1] + ' + ' + line[2] + '; ' + '\n')
+            f.write('Operation: $' + line[0] + ' = ' + '$' + line[1] + ' + ' + line[2] + '; ' + '\n')
             f.write('PC is now at ' + str(PC) + '\n')
             f.write('Registers that have changed: ' + '$' + line[0] + ' = ' + str(regval[int(line[0])]) + '\n')
 
-        elif(line[0:3] == "add"): # ADD
+        elif(line[0:3] == "add"): # ADD $d = $s + $t; advance_pc (4); add $d, $s, $t
             line = line.replace("add","")
             line = line.split(",")
-            rd = format(int(line[0]),'05b')
-            rs = format(int(line[1]),'05b')
-            rt = format(int(line[2]),'05b')
-            f.write(str('000000') + str(rs) + str(rt) + str(rd) + str('00000100000') + '\n')
+            PC = PC + 4;
+            regval[int(line[0])] = regval[int(line[1])] + regval[int(line[2])]
+            f.write('Operation: $' + line[0] + ' = ' + '$' + line[1] + ' + $' + line[2] + '; ' + '\n')
+            f.write('PC is now at ' + str(PC) + '\n')
+            f.write('Registers that have changed: ' + '$' + line[0] + ' = ' + str(regval[int(line[0])]) + '\n')
             
-        elif(line[0:5] == "addiu"): # ADD
-            line = line.replace("addiu","")
-            line = line.split(",")
-            rs = format(int(line[1]),'05b')
-            rt = format(int(line[0]),'05b')
-            imm = format(int(line[2]),'016b') if (int(line[2]) > 0) else format(65536 + int(line[2]),'016b')
-            f.write(str('001001') + str(rs) + str(rt) + str(imm) + '\n')
         
-        elif(line[0:5] == "multu"): # ADD
+        elif(line[0:5] == "multu"): # $LO = $s * $t; advance_pc (4); mult $s, $t
             line = line.replace("multu","")
             line = line.split(",")
-            rs = format(int(line[0]),'05b')
-            rt = format(int(line[1]),'05b')
-            f.write(str('000000') + str(rs) + str(rt) + str('0000000000011001') + '\n')
+            PC = PC + 4;
+            regval[LO] = regval[int(line[0])]*regval[int(line[1])]
+            f.write('Operation: $LO' + ' = ' + '$' + line[0] + ' * $' + line[1] + '; ' + '\n')
+            f.write('PC is now at ' + str(PC) + '\n')
+            f.write('Registers that have changed: ' + '$LO = ' + str(regval[LO]) + '\n')
             
         #mult
-        elif(line[0:4] == "mult"): # ADD
+        elif(line[0:4] == "mult"): # $LO = $s * $t; advance_pc (4); mult $s, $t
             line = line.replace("mult","")
             line = line.split(",")
-            rs = format(int(line[0]),'05b')
-            rt = format(int(line[1]),'05b')
-            f.write(str('000000') + str(rs) + str(rt) + str('0000000000011000') + '\n')
+            PC = PC + 4;
+            regval[LO] = regval[int(line[0])]*regval[int(line[1])]
+            f.write('Operation: $LO' + ' = ' + '$' + line[0] + ' * $' + line[1] + '; ' + '\n')
+            f.write('PC is now at ' + str(PC) + '\n')
+            f.write('Registers that have changed: ' + '$LO = ' + str(regval[LO]) + '\n')
             
         #srl
         elif(line[0:3] == "srl"): # ADD
@@ -186,19 +200,27 @@ def main():
             line = line.replace("j","")
             line = line.split(",")
 
+            f.write('Operation: PC = nPC; ' + '\n')
+
+
             # Since jump instruction has 2 options:
             # 1) jump to a label
             # 2) jump to a target (integer)
             # We need to save the label destination and its target location
 
             if(line[0].isdigit()): # First,test to see if it's a label or a integer
-                f.write(str('000010') + str(format(int(line[0]),'026b')) + '\n')
+                 PC = line[0]
+                 line = line[0]
+                 f.write('PC is now at ' + str(line[0]) + '\n')
 
             else: # Jumping to label
                 for i in range(len(labelName)):
                     if(labelName[i] == line[0]):
-                        f.write(str('000010') + str(format(int(labelIndex[i]),'026b')) + '\n')
-
+                        PC = labelAddr[i]
+                        line = labelIndex[i]
+                        f.write('PC is now at ' + str(labelAddr[i]) + '\n')
+                
+            f.write('No Registers have changed. \n')
 
 
     f.close()
